0d4c09d423dc938aba7b2330a35455a9bf0eb507
Vince van Noort <vincevannoort@gmail.com>
2018-03-28 11:54:41 +0200

    added wifi and mqtt

---
+++ node-mcu/env-variables-example.h
@@ -0,0 +1,23 @@
+#ifndef WLAN_SSID
+#define WLAN_SSID "example"
+#endif
+
+#ifndef WLAN_PASS
+#define WLAN_PASS "example"
+#endif
+
+#ifndef AIO_SERVER
+#define AIO_SERVER "example"
+#endif
+
+#ifndef AIO_SERVERPORT
+#define AIO_SERVERPORT 0
+#endif
+
+#ifndef AIO_USERNAME
+#define AIO_USERNAME "example"
+#endif
+
+#ifndef AIO_KEY
+#define AIO_KEY "example"
+#endif
--- node-mcu/node-mcu.ino
+++ node-mcu/node-mcu.ino
@@ -1,24 +1,22 @@
-// #include <OLEDDisplay.h>
-// #include <OLEDDisplayFonts.h>
-// #include <OLEDDisplayUi.h>
-// #include <SH1106.h>
-// #include <SH1106Spi.h>
-// #include <SH1106Wire.h>
-// #include <SSD1306.h>
-// #include <SSD1306Spi.h>
+#include "env-variables.h"
+
+// OLED
 #include <SSD1306Wire.h>
+
+
+// WIFI AND MQTT
+#include <ESP8266WiFi.h>
+#include <Adafruit_MQTT.h>
+#include <Adafruit_MQTT_Client.h>
+WiFiClientSecure client;
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+Adafruit_MQTT_Publish photocell = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/photocell");
+void MQTT_connect();
 
 /**
  * Soil Moisture sensor
  */
 class soil_moisture_sensor {
-
-};
-
-/**
- * Servo motor
- */
-class servo_motor {
 
 };
 
@@ -31,22 +29,62 @@ class oled_display {
 };
 
 void setup() {
+  Serial.begin(9600);
+
+  // OLED
   ssd1306_oled_display.init();
   ssd1306_oled_display.flipScreenVertically();
   ssd1306_oled_display.setFont(ArialMT_Plain_10);
 
+  // WIFI AND MQTT
+  Serial.begin(115200);
+  delay(10);
+  Serial.println(F("Adafruit MQTT demo"));
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+  mqtt.subscribe(&onoffbutton);
+
+  // LED
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
+  Serial.print(WIFI_PASSWORD);
   ssd1306_oled_display.clear();
   ssd1306_oled_display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
   ssd1306_oled_display.setFont(ArialMT_Plain_16);
   ssd1306_oled_display.drawString(64, 32, "status: analysing");
   ssd1306_oled_display.display();
 
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(5000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(5000);
+  // MQTT
+  MQTT_connect();
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(5000))) {
+    if (subscription == &onoffbutton) {
+      Serial.print(F("Got: "));
+      Serial.println((char *)onoffbutton.lastread);
+    }
+  }
+  Serial.print(F("\nSending photocell val "));
+  Serial.print(x);
+  Serial.print("...");
+  if (! photocell.publish(x++)) {
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("OK!"));
+  }
+
+  // digitalWrite(LED_BUILTIN, HIGH);
+  // delay(5000);
+  // digitalWrite(LED_BUILTIN, LOW);
+  // delay(5000);
 }
